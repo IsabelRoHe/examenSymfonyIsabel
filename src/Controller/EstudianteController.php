35154571be7617566e9469ebc3b350032a1b57c8
Isabel Rodriguez Herranz <isabelrodriguez@ics.local>
2021-09-22 20:09:23 +0200

    first commit

---
+++ src/Controller/EstudianteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EstudianteRepository;
+use App\Repository\NotaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EstudianteController extends AbstractController
+{
+    #[Route('/estudiante', name: 'estudiantes_index', methods: ['GET'])]
+    public function index(EstudianteRepository $estudianteRepository): Response
+    {
+
+        $estudiantes = $estudianteRepository->findAll();
+
+        return $this->render('estudiante/index.html.twig', [
+            'estudiantes' => $estudiantes,
+        ]);
+    }
+
+    #[Route('/estudiante/{id}/notas', name: 'estudiante_notas', requirements: ['id' => '\d+'] , methods: ['GET'])]
+    public function notas($id, EstudianteRepository $estudianteRepository, NotaRepository $notaRepository): Response
+    {
+
+        $estudiante = $estudianteRepository->find($id);
+        $notas = $notaRepository->findBy(['estudiante' => $id]);
+
+        return $this->render('estudiante/notas.html.twig', [
+            'estudiante' => $estudiante,
+            'notas' => $notas,
+        ]);
+    }
+}
---
+++ src/Entity/Nota.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NotaRepository::class)
+ */
+class Nota
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Estudiante::class, inversedBy="notas")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $estudiante;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Curso::class, inversedBy="notas")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $curso;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $nota;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEstudiante(): ?Estudiante
+    {
+        return $this->estudiante;
+    }
+
+    public function setEstudiante(?Estudiante $estudiante): self
+    {
+        $this->estudiante = $estudiante;
+
+        return $this;
+    }
+
+    public function getCurso(): ?Curso
+    {
+        return $this->curso;
+    }
+
+    public function setCurso(?Curso $curso): self
+    {
+        $this->curso = $curso;
+
+        return $this;
+    }
+
+    public function getNota(): ?float
+    {
+        return $this->nota;
+    }
+
+    public function setNota(float $nota): self
+    {
+        $this->nota = $nota;
+
+        return $this;
+    }
+}
